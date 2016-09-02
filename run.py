9ba51c7c5b2c2815e526bc1986788c317e76d7f3
Alexandre Decan <alexandre.decan@lexpage.net>
2016-09-02 15:01:49 +0200

    Put PyQt5 in rqeuirements (needs Python3.5+).

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ from pytbul.gui import main
 if __name__ == '__main__':
     argv = sys.argv
 
-    if '-style' not in argv:
-        argv.append('-style')
-        argv.append('adwaita')
+    #if '-style' not in argv:
+    #    argv.append('-style')
+    #    argv.append('adwaita')
 
     main(argv)
 
